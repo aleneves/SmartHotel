3088e1ab9f10ae49d730963dbc4dae40ed4fa4d8
Alexandre Simoes Neves <ale.neves@outlook.com>
2025-10-16 14:08:25 -0300

    inicial

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,75 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import model.exceptions.DomainException;
+
+public class Reservation {
+	
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if(!checkOut.after(checkIn)) {
+			throw new DomainException("Data do check-out deve ser maior que data do check-in.");
+		}
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}	
+	
+	private long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
+		return diff;
+	}
+	
+	public void updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if(checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Data reservadas devem ser futuras.");
+		}
+		if(!checkOut.after(checkIn)) {
+			throw new DomainException("Data do check-out deve ser maior que data do check-in.");
+		}
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	@Override
+	public String toString() {
+		return "Room "
+				+ roomNumber
+				+ ", check-in: "
+				+ sdf.format(checkIn)
+				+ ", check-out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " nights";
+	}
+	
+	
+	
+}
